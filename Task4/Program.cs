15f97f91801828910c60210e63e524ba78140a2e
Дарья Кузнецова <dakuznetsova_3@edu.hse.ru>
2019-05-22 23:19:38 +0500

    readanswer add

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -12,18 +12,19 @@ namespace Task4
         {
             double x0 = 0;
             double x1 = Math.PI/4;
-            double e = 0.00000000001;
+            Console.WriteLine("Введите точность е");
+            double e = ReadAnswer();
             double x = method_chord(x0, x1, e);
-            Console.WriteLine("{0:0.000000000000000000000}",x);
-            x = method_chord(x0, x1, 5);
-            Console.WriteLine(x);
-            x = method_chord(x0, x1, 0.01);
-            Console.WriteLine(x);
-            x = method_chord(x0, x1, 0.001);
-            Console.WriteLine(x);
-            x = method_chord(x0, x1, 0.0001);
-            Console.WriteLine(x);
-            Console.ReadLine();
+            Console.WriteLine($"заданная точность: {e} x={x}");
+            e = 1;
+            for(int i=0; i<5;i++)
+            {
+                x = method_chord(x0, x1, e);
+                Console.WriteLine($"заданная точность: {e} x={x}");
+                e *= 0.01;
+            }
+            x = method_chord(x0, x1, 0);
+            Console.WriteLine($"заданная точность: {0} x={x}");
         }
 
         public static double method_chord(double x_prev, double x_curr, double e)
@@ -33,7 +34,7 @@ namespace Task4
             do
             {
                 tmp = x_next;
-                x_next = x_curr - f(x_curr) * (x_prev - x_curr) / (f(x_prev) - f(x_curr));
+                x_next = x_curr - f(x_curr) * (x_curr - x_prev) / (f(x_curr) - f(x_prev));
                 x_prev = x_curr;
                 x_curr = tmp;
             } while (Math.Abs(x_next - x_curr) > e);
@@ -44,6 +45,28 @@ namespace Task4
         public static double f(double x)
         {
             return (2 * Math.Pow(Math.Sin(2 * x), 2)) / 3 - (3 * Math.Pow(Math.Cos(2 * x), 2)) / 4;
+        }
+
+        public static double ReadAnswer()
+        {
+            double a = 0;
+            bool ok = false;
+            do
+            {
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    if (a >= 0)
+                        ok = true;
+                    else Console.WriteLine("e должно быть положительным числом");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Пожалуйста, введите действительное число.");
+                    ok = false;
+                }
+            } while (!ok);
+            return a;
         }
     }
 }
